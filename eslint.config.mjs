ab3bbbe14369ed4a52f4fd3a040dfd6d1303b8c0
i-am-smolli <zpoolsde@gmail.com>
2025-07-14 12:23:20 +0200

    lint the linter file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,11 +4,17 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { ignores: ["node_modules/", ".next/"], },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser }, },
+  { ignores: ["node_modules/", ".next/"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
   {
     ...pluginReact.configs.flat.recommended,
     settings: {
