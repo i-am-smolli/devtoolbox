f8bdb18206c1a66666f21bcb0b734000af2a1462
i-am-smolli <zpoolsde@gmail.com>
2025-07-16 09:36:17 +0200

    prepare manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,21 @@
+// Once we decide to use PWA features, we can uncomment this code
+// import type { MetadataRoute } from 'next'
+//  
+// export default function manifest(): MetadataRoute.Manifest {
+//   return {
+//     name: 'DevToolbox - Developer Utilities & Tools"',
+//     short_name: 'DevToolbox',
+//     description: 'Next.js App',
+//     start_url: '/',
+//     display: 'standalone',
+//     background_color: '#282c34',
+//     theme_color: '#4285F4',
+//     icons: [
+//       {
+//         src: '/favicon.svg',
+//         sizes: 'any',
+//         type: 'image/x-icon',
+//       },
+//     ],
+//   }
+// }
