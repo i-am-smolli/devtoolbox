1eb554c444335ed46d971a3d4afcc428b05fd0be
i-am-smolli <zpoolsde@gmail.com>
2025-07-29 07:41:58 +0200

    remove unused

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -128,13 +128,5 @@ SheetDescription.displayName = SheetPrimitive.Description.displayName;
 
 export {
   Sheet,
-  SheetPortal,
-  SheetOverlay,
-  SheetTrigger,
-  SheetClose,
   SheetContent,
-  SheetHeader,
-  SheetFooter,
-  SheetTitle,
-  SheetDescription,
 };
